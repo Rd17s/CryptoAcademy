b618d4dee6d88107dbc6b5992b6dd421ce5a98a3
Rd17s <102297186+Rd17s@users.noreply.github.com>
2022-03-24 16:44:38 +0500

    Create Rd.js

---
+++ Rd.js
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main()
+{
+	// 1. prompt user to enter coefficients
+
+	// 2. read coefficients for equation `ax + b = 0`
+    
+	// 3. solve equation `ax + b = 0`
+	// solution: `x = -b / a`
+}
